43ac8bbebfe65fb700eda02b317ecb3b1517e191
zjeffreys <zjeffreys@seattlu.edu>
2023-04-07 10:17:17 -0700

    final orange changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ import styles from "../styles/index.module.css";
 const Header = () => {
   return (
     <React.Fragment>
-    <p className={styles.quote}>"Be The Main Character In Your Own Story!"</p>
+      <img src="/StoryLandLogo.png" className={styles.icon} />
+    {/* <p className={styles.quote}>"Be The Main Character In Your Own Story!"</p> */}
     <img src="/avatar.png" className={styles.icon} />
   </React.Fragment>
   );
